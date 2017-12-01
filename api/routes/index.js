0dcf5dfe01e858bb663978b8a32c21e13c15633f
Rudy Rodriguez <rudy1405@hotmail.com>
2017-11-30 19:05:36 -0600

    routes added

--- api/routes/index.js
+++ api/routes/index.js
@@ -14,8 +14,9 @@ module.exports = (app) => { // declaracion para exportar el modulo
     todoList_routes.put('/:taskId',todoList_controller.updateTask) // X2
     todoList_routes.delete('/:taskId',todoList_controller.deleteTask) ///X3
 
-    auth_routes.post('/register', authentication_controller.register)
-    auth_routes.post('/login', authentication_controller.login)
+    
+    todoList_routes.post('/register', authentication_controller.register)
+    todoList_routes.post('/login', authentication_controller.login)
 
     app.use ('/api', api_routes)    // Cuando se llega aqui se usa la ruta /API y la funcion apiroutes que es esta
 }// end module.exports
